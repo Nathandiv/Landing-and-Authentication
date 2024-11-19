983a293a105654c22e783b44c1c8bdeba1a59575
Nathandiv <k.n.hlungwani@gmail.com>
2024-11-19 15:47:47 +0200

    added landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
@@ -5,14 +6,15 @@ import { initFlowbite } from 'flowbite';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   title = 'AuthZone';
-
+  screen = window.innerWidth
   ngOnInit(): void {
+
     initFlowbite();
   }
 
